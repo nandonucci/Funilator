33db53857554f7376dd8830015d23cac9b4452c7
Marcos Vaz <marcosvaz_contato@outlook.com>
2018-05-18 16:49:17 -0300

    Melhorada ordem de funcoes

--- admin/includes/functions.php
+++ admin/includes/functions.php
@@ -1,7 +1,52 @@
 <?php
   include 'db.php';
 
-  //Inserir usuário
+  // Função de login do usuário
+  function logar()
+  {
+    global $connection;
+    if (isset($_POST['login'])) {
+      $username = $_POST['email'];
+      $password = $_POST['password'];
+
+      $username = mysqli_real_escape_string($connection, $username);
+      $password = mysqli_real_escape_string($connection, $password);
+
+      $query = "SELECT * FROM fun_usuario WHERE  ds_email = '$username'";
+      $select_usuario = mysqli_query($connection, $query);
+
+      while ($row = mysqli_fetch_assoc($select_usuario)) {
+        $db_id = $row['id'];
+        $db_email = $row['ds_email'];
+        $db_password = $row['ds_senha'];
+        $db_user = $row['nm_usuario'];
+      }
+
+      echo $username . "=" . $db_email . "?<br>";
+
+      echo $password . "=" . $db_password . "?";
+      if ($username !== $db_email && $password !== $db_password) {
+
+        header('Location: acesso.php');
+
+      } elseif ($username == $db_email && $password == $db_password) {
+
+        // $_SESSION['id'] = $db_id;
+        $_SESSION['ds_email'] = $db_email;
+        $_SESSION['password'] = $db_password;
+        $_SESSION['username'] = $db_user;
+
+        header('Location: index.php');
+
+      } else {
+
+        header('Location: acesso.php');
+
+      }
+    }
+  }
+
+  // Função de cadastro de usuário
   function novoUsuario()
   {
     global $connection;
@@ -46,7 +91,7 @@
     }
   }
 
-  //Função atualiza cadastro
+  //Função de atualizar cadastro
   function atualizaCadastro()
   {
     global $connection;
@@ -84,50 +129,6 @@
 
     } elseif (isset($_POST['cancelar'])) {
       header('Location: ../admin/index.php');
-    }
-  }
-
-  function logar()
-  {
-    global $connection;
-    if (isset($_POST['login'])) {
-      $username = $_POST['email'];
-      $password = $_POST['password'];
-
-      $username = mysqli_real_escape_string($connection, $username);
-      $password = mysqli_real_escape_string($connection, $password);
-
-      $query = "SELECT * FROM fun_usuario WHERE  ds_email = '$username'";
-      $select_usuario = mysqli_query($connection, $query);
-
-      while ($row = mysqli_fetch_assoc($select_usuario)) {
-        $db_id = $row['id'];
-        $db_email = $row['ds_email'];
-        $db_password = $row['ds_senha'];
-        $db_user = $row['nm_usuario'];
-      }
-
-      echo $username . "=" . $db_email . "?<br>";
-
-      echo $password . "=" . $db_password . "?";
-      if ($username !== $db_email && $password !== $db_password) {
-
-        header('Location: acesso.php');
-
-      } elseif ($username == $db_email && $password == $db_password) {
-
-        // $_SESSION['id'] = $db_id;
-        $_SESSION['ds_email'] = $db_email;
-        $_SESSION['password'] = $db_password;
-        $_SESSION['username'] = $db_user;
-
-        header('Location: index.php');
-
-      } else {
-
-        header('Location: acesso.php');
-
-      }
     }
   }
 
